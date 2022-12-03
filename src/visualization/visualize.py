a84be3356c84c1dbdfbec8b0ff1ec6ccd52b5237
danielcson <daniel.c.son@gmail.com>
2022-12-02 19:57:48 -0800

    Update visualize.py

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -77,4 +77,4 @@ def plot_cka_fig():
     df_c = pd.DataFrame(figure_5c).pivot('activation_1', 'activation_2', 'cka')
     sns.heatmap(df_c, cbar_kws={'label':'Representational similarity (CKA)'}, cmap="Blues")
 
-    return loaded_hook_dict, total_kinematic_dict
+    return sns.heatmap(df_b, cbar_kws={'label':'Feature encoding (CKA)'}, cmap="Blues")
