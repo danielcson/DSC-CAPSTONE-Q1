5be722c5d1b12dfd3b94e6e6d7e91373a1ed69ff
danielcson <daniel.c.son@gmail.com>
2022-12-02 19:59:45 -0800

    s

--- src/model/train_agent.py
+++ src/model/train_agent.py
@@ -82,7 +82,7 @@ def extract_kinematic_activations():
 
 
     # run a few episodes just to collect activations
-    num_episodes_to_run = 25
+    num_episodes_to_run = 5
 
     # for recording kinematics
     total_kinematic_dict = {
