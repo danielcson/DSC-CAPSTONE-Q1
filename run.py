af324b4c2399f8aa20caeeb454bb6812bfb31db3
Daniel Son <61219767+danielcson@users.noreply.github.com>
2022-12-04 21:42:06 -0800

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import subprocess
 
 print('run on colab for more reliable results')
 print('https://colab.research.google.com/drive/1TDih5q6UMmi-ZFNT5OzUmC6dySQOfhXo?usp=sharing')
-subprocess.call("src/model/train_agent.py")
+subprocess.call("dsc_capstone_q1/src/model/train_agent.py")
