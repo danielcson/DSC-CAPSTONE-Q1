b01ae70fd635020522bba44c380ace4629c91aa5
Daniel Son <61219767+danielcson@users.noreply.github.com>
2022-12-04 21:29:05 -0800

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ import subprocess
 
 if __name__ == "main":
     print('run on colab for more reliable results')
+    print('https://colab.research.google.com/drive/1TDih5q6UMmi-ZFNT5OzUmC6dySQOfhXo?usp=sharing')
     subprocess.call("src/model/train_agent.py")
